2bde041693859bd465418688c482cb79a5c62ed0
JoseFernandes975 <zehenrique752005@gmail.com>
2023-02-12 20:33:22 -0300

    Product insert, update, delete

--- backend/src/main/java/com/fernandesjose/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/fernandesjose/dscatalog/resources/ProductResource.java
@@ -1,19 +1,29 @@
 package com.fernandesjose.dscatalog.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fernandesjose.dscatalog.dto.ProductDTO;
-import com.fernandesjose.dscatalog.entities.Product;
+import com.fernandesjose.dscatalog.exceptions.ResourceNotFoundException;
 import com.fernandesjose.dscatalog.services.ProductService;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -38,6 +48,25 @@ public class ProductResource {
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
 		ProductDTO dto = services.findById(id);
 		return ResponseEntity.ok(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+		dto = services.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+		dto = services.update(id, dto);
+		return ResponseEntity.ok(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> delete(@PathVariable Long id){
+		services.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- backend/src/main/java/com/fernandesjose/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/fernandesjose/dscatalog/services/ProductService.java
@@ -1,23 +1,35 @@
 package com.fernandesjose.dscatalog.services;
 
+import java.time.Instant;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fernandesjose.dscatalog.dto.CategoryDTO;
 import com.fernandesjose.dscatalog.dto.ProductDTO;
+import com.fernandesjose.dscatalog.entities.Category;
 import com.fernandesjose.dscatalog.entities.Product;
+import com.fernandesjose.dscatalog.exceptions.DataBaseException;
 import com.fernandesjose.dscatalog.exceptions.ResourceNotFoundException;
+import com.fernandesjose.dscatalog.repositories.CategoryRepository;
 import com.fernandesjose.dscatalog.repositories.ProductRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
 	
 	@Autowired
 	private ProductRepository repo;
+	
+	@Autowired
+	private CategoryRepository catRepo;
 	
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAll(PageRequest pageRequest){
@@ -31,5 +43,49 @@ public class ProductService {
 		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found!"));
 		return new ProductDTO(entity, entity.getCategories());
 	}
-
+	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		transformation(entity, dto);
+        entity = repo.save(entity);
+        return new ProductDTO(entity);
+	}
+	
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+	try {
+		Product entity = repo.getReferenceById(id);
+		transformation(entity, dto);
+		entity = repo.save(entity);
+		return new ProductDTO(entity);
+	}catch(EntityNotFoundException e) {
+		throw new ResourceNotFoundException("Id não encontrado!");
+	}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repo.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found!");
+		}catch(DataIntegrityViolationException e) {
+			throw new DataBaseException("Dont´t delete this product, it is associated!");
+		}
+	}
+	
+	private void transformation(Product entity, ProductDTO dto) {
+		entity.setName(dto.getName());
+		entity.setPrice(dto.getPrice());
+		entity.setDescription(dto.getDescription());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setDate(dto.getDate());
+		entity.getCategories().clear();
+		
+		for(CategoryDTO x : dto.getCategories()) {
+			Category cat = catRepo.getReferenceById(x.getId());
+			entity.getCategories().add(cat);
+		}
+		
+	}
 }
