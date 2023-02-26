ed85ecd75a7f35463cfe3932424eed03cdbef23d
JoseFernandes975 <zehenrique752005@gmail.com>
2023-02-26 18:49:45 -0300

    Refactor Pageable

--- backend/src/main/java/com/fernandesjose/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/fernandesjose/dscatalog/resources/ProductResource.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -33,14 +34,8 @@ public class ProductResource {
 	private ProductService services;
 	
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findAllPage(
-	@RequestParam(value = "page", defaultValue = "0") Integer page,
-	@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-	@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-	@RequestParam(value = "orderBy", defaultValue = "name") String orderBy){
-		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		Page<ProductDTO> list = services.findAll(pageRequest);
+	public ResponseEntity<Page<ProductDTO>> findAllPage(Pageable pageable){
+		Page<ProductDTO> list = services.findAll(pageable);
 		return ResponseEntity.ok(list);
 	}
 	
