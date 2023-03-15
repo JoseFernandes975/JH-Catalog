d34e87d2b550d6fbc1002b9276ba1c1b7b761424
JoseFernandes975 <zehenrique752005@gmail.com>
2023-03-15 11:33:50 -0300

    Custom validation response

--- backend/src/main/java/com/fernandesjose/dscatalog/resources/exception/ResourceExceptionHandler.java
+++ backend/src/main/java/com/fernandesjose/dscatalog/resources/exception/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -37,5 +39,24 @@ public class ResourceExceptionHandler {
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		ValidationError error = new ValidationError();
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError("Validation Exception!");
+		error.setMessage(e.getMessage());
+		
+		//Para diminuir o string muito grande que está dando na resposta http, agora vai deixar a mensagem menos verboso
+		for(FieldError f : e.getBindingResult().getFieldErrors()) {
+			error.addErrors(f.getField(), f.getDefaultMessage());
+		}
+		
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+		
 	}
 }
