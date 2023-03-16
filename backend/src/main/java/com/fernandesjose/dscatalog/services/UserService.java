750d620675c1a92a0dcbf8304cc9629b563d925d
JoseFernandes975 <zehenrique752005@gmail.com>
2023-03-16 13:34:28 -0300

    Spring Security for web

--- backend/src/main/java/com/fernandesjose/dscatalog/services/UserService.java
+++ backend/src/main/java/com/fernandesjose/dscatalog/services/UserService.java
@@ -2,11 +2,16 @@ package com.fernandesjose.dscatalog.services;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +28,9 @@ import com.fernandesjose.dscatalog.repositories.RoleRepository;
 import com.fernandesjose.dscatalog.repositories.UserRepository;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -85,5 +92,16 @@ public class UserService {
 			entity.getRoles().add(role);
 		}
 		
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if(user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found!");
+		}
+		logger.info("Email found: "+username);
+		return user;
 	}
 }
