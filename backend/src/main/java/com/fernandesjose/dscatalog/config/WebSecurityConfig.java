e5c42efd2a8e0ea2d71dc910f74028201e67bfcf
JoseFernandes975 <zehenrique752005@gmail.com>
2023-03-16 15:28:44 -0300

    Implementing Authorization Server OAuth2

--- backend/src/main/java/com/fernandesjose/dscatalog/config/WebSecurityConfig.java
+++ backend/src/main/java/com/fernandesjose/dscatalog/config/WebSecurityConfig.java
@@ -1,16 +1,45 @@
 package com.fernandesjose.dscatalog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+    private	BCryptPasswordEncoder encoder;
+	
+	@Autowired
+	private UserDetailsService detailsUser;
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//O Spring Security quando for fazer a autenticação sabe como vai buscar o user por email que é por meio desse service
+		// e como vai ter que analisar a senha criptografada com BCrypt
+		auth.userDetailsService(detailsUser).passwordEncoder(encoder);
+	}
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**");
+		web.ignoring().antMatchers("/actuator/**");
 	}
+
+	@Override
+	@Bean
+	//Vamos precisar dele para implementar o AuthorizationServer
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	
 }
