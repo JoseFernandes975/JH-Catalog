b4f4bffa218be0b4db3f44a2726462c24efa8a0d
JoseFernandes975 <zehenrique752005@gmail.com>
2023-03-20 11:51:52 -0300

    H2 security configuration

--- backend/src/main/java/com/fernandesjose/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/fernandesjose/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.fernandesjose.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -14,10 +17,13 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	@Autowired
+	private Environment env;
+	
+	@Autowired
 	private TokenStore tokenStore;
 	
 	//definindo quais requisições vão ser acessadas por quem
-	private static final String[] PUBLIC = { "/oauth/token" };
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
 	private static final String[] ADMIN = {"/users/**"};
 
@@ -28,6 +34,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		//Mostrar o h2
+		//No ambiente da aplicação, se os profiles que estiver sendo executado e for test
+		//desabilitar os frames pois a interface do h2 quer que desbilite os frames
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+		
 		//Configurando as requisições autorizadas a quem
 		http.authorizeRequests()
 		//O vetor PUBLIC essas requisição vão ser permitido por todos
